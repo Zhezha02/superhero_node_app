03bb240e5d8322001e454c14d974184a5963bcfd
Evgeniya Kirtoka <evgeniyakirtoka@gmail.com>
2021-04-07 20:21:20 +0300

    Fix app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = require('./routers');
 const errorHandler = require('./middlewares/error.handlers');
 
-const app = express;
+const app = express();
 
 app.use(express.json());
 app.use('/api', router);
